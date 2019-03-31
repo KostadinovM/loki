afd9cc2abf6b36ad553918ec6ed088d875bd2ce2
KostadinovM <spotted@abv.bg>
2019-03-31 19:37:52 +0200

    camera class

--- loki/src/graphics/camera.cpp
+++ loki/src/graphics/camera.cpp
@@ -1,4 +1,8 @@
 #include "camera.h"
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
 namespace Loki
 {
 	namespace Graphics
@@ -9,10 +13,14 @@ namespace Loki
 
 		Camera::Camera(glm::vec3 position, glm::vec3 forward, glm::vec3 up)
 		{
+			Position = position;
+			Forward = forward;
+			Up = up;
 		}
 
 		void Camera::UpdateViewMatrix()
 		{
+			View = glm::lookAt(Position, Position + Forward, Up);
 		}
 	}
 }
--- loki/src/graphics/camera.h
+++ loki/src/graphics/camera.h
@@ -1,4 +1,5 @@
 #include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
 
 namespace Loki
 {
@@ -7,13 +8,13 @@ namespace Loki
 		class Camera
 		{
 		private:
-			glm::vec3 Position;
-			glm::vec3 Front;
-			glm::vec3 Up;
-			glm::vec3 Right;
-			glm::vec3 WorldUp;
 
 		public:
+			glm::vec3 Position;
+			glm::vec3 Forward;
+			glm::vec3 Up;
+			glm::mat4 View;
+
 			Camera();
 			Camera(glm::vec3 position, glm::vec3 forward, glm::vec3 up);
 			void UpdateViewMatrix();

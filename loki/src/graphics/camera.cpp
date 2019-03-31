bb3da58fe781ba6ce9bd3fed990f8de011177a6f
KostadinovM <spotted@abv.bg>
2019-03-31 12:11:45 +0200

    camera class work

---
+++ loki/src/graphics/camera.cpp
@@ -0,0 +1,18 @@
+#include "camera.h"
+namespace Loki
+{
+	namespace Graphics
+	{
+		Camera::Camera()
+		{
+		}
+
+		Camera::Camera(glm::vec3 position, glm::vec3 forward, glm::vec3 up)
+		{
+		}
+
+		void Camera::UpdateViewMatrix()
+		{
+		}
+	}
+}
--- loki/src/graphics/camera.h
+++ loki/src/graphics/camera.h
@@ -1,3 +1,5 @@
+#include <glm/vec3.hpp>
+
 namespace Loki
 {
 	namespace Graphics
@@ -5,9 +7,16 @@ namespace Loki
 		class Camera
 		{
 		private:
+			glm::vec3 Position;
+			glm::vec3 Front;
+			glm::vec3 Up;
+			glm::vec3 Right;
+			glm::vec3 WorldUp;
 
 		public:
-
+			Camera();
+			Camera(glm::vec3 position, glm::vec3 forward, glm::vec3 up);
+			void UpdateViewMatrix();
 		};
 	}
 }

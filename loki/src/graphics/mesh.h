163d2b887477840fdef2047cbdad5161bc71f170
KostadinovM <spotted@abv.bg>
2019-04-10 12:37:38 +0200

    mesh class

---
+++ loki/src/graphics/mesh.h
@@ -0,0 +1,42 @@
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <vector>
+#include <string>
+#include <shader.h>
+#include <texture.h>
+
+namespace Loki
+{
+	namespace Graphics
+	{
+		struct Vertex
+		{
+			glm::vec3 position;
+			glm::vec3 normal;
+			glm::vec2 uv;
+		};
+
+		class Mesh
+		{
+		private:
+			GLuint VAO, VBO, EBO;
+			std::vector<Vertex> vertices;
+			std::vector<GLuint> indices;
+			std::vector<Texture> textures;
+
+		public:
+
+			Mesh();
+			~Mesh();
+			Mesh(std::vector<Vertex> vertices, std::vector<GLuint> indices);
+			Mesh(std::vector<Vertex> vertices, std::vector<GLuint> indices, std::vector<Texture> textures);
+
+			void draw(Shader shader);
+
+		private:
+			void setupMesh();
+		};
+
+
+	}
+}
